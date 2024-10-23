89797d61d315c11346b80758a5447329eaa6904c
hanh00740 <hanh00740@gmail.com>
2024-10-23 20:23:55 +0900

    Update camp.py

--- camp.py
+++ camp.py
@@ -23,6 +23,7 @@ chrome_options.add_argument("--headless")  # Run in headless mode (no UI)
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--disable-gpu') 
 chrome_options.add_argument("--window-size=1920,1080")
 #chrome_options.setPage
 #chrome_options.add_experimental_option("detach", True)
